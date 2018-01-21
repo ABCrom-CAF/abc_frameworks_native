SOKP/android_frameworks_native

input: Adjust priority
Change-Id: Ic0477225e9b0bb3eb4f2c71ab3b2620f56d947c7
Signed-off-by: Hriday Sharma <hridaysharma42@gmail.com>

services/surfaceflinger/SurfaceFlinger_hwc1.cpp
@@ -30,6 +30,7 @@
 
 #include <EGL/egl.h>
 
+#include <cutils/iosched_policy.h>
 #include <cutils/log.h>
 #include <cutils/properties.h>
 
@@ -555,6 +556,7 @@ void SurfaceFlinger::init() {
 
     mEventControlThread = new EventControlThread(this);
     mEventControlThread->run("EventControl", PRIORITY_URGENT_DISPLAY);
+    android_set_rt_ioprio(mEventControlThread->getTid(), 1);
 
     // set a fake vsync period if there is no HWComposer
     if (mHwc->initCheck() != NO_ERROR) {

ABCrom-CAF/abc_frameworks_native

services/surfaceflinger/SurfaceFlinger_hwc1.cpp
@@ -28,6 +28,7 @@
 
 #include <EGL/egl.h>
 
+#include <bfqio/bfqio.h>
 #include <cutils/properties.h>
 #include <log/log.h>
 
@@ -585,6 +586,7 @@ void SurfaceFlinger::init() {
 
     mEventControlThread = new EventControlThread(this);
     mEventControlThread->run("EventControl", PRIORITY_URGENT_DISPLAY);
+    android_set_rt_ioprio(mEventControlThread->getTid(), 1);
 
     // set a fake vsync period if there is no HWComposer
     if (mHwc->initCheck() != NO_ERROR) {